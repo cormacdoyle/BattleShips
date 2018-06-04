c05dbc9948058025882105da4aa14619f34ba85f
Cormac <cormacdoyle802@gmail.com>
2018-06-04 08:15:05 -0400

    poo

--- PycharmProjects/Computer science/CPT.py
+++ PycharmProjects/Computer science/CPT.py
@@ -22,24 +22,23 @@ shipfinalcolour = (0, 200, 255)
 myfont = pygame.font.SysFont('arial bold', 30)
 textsurface1 = myfont.render('Your score: ', False, (0, 255, 0))  # add in score keeping variable
 textsurface2 = myfont.render('Enemy score: ', False, (0, 255, 0))
-gridname1= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+textsurface3 = myfont.render('A      B      C      D      E      F      G      H      I      J', False, black)
 
+enemyship_listx = [50,100,150,200,250,300,350,400,450,500]
+enemyship_listy = [50,100,150,200,250,300,350,400,450,500]
 
-randomnum1=(random.randint(0, 10))
-randomnum2=(random.randint(0, 10))
-randomnum3=(random.randint(0, 10))
-randomnum4=(random.randint(0, 9))
-randomnum5=(random.randint(0, 10))
-randomnum6=(random.randint(0, 8))
-randomnum7=(random.randint(0, 10))
-randomnum8=(random.randint(0, 10))
-randomnum9=(random.randint(0, 9))
-randomnum10=(random.randint(0, 10))
-randomnum11=(random.randint(0, 8))
-randomnum12=(random.randint(0, 10))
-
-enemyship_listx = [50,100,150,200,250,300,350,400,450,500,550]
-enemyship_listy = [50,100,150,200,250,300,350,400,450,500,550]
+randomnum1=(random.randint(0, 8))
+randomnum2=(random.randint(0, 8))
+randomnum3=(random.randint(0, 8))
+randomnum4=(random.randint(0, 7))
+randomnum5=(random.randint(0, 8))
+randomnum6=(random.randint(0, 6))
+randomnum7=(random.randint(0, 8))
+randomnum8=(random.randint(0, 8))
+randomnum9=(random.randint(0, 6))
+randomnum10=(random.randint(0, 8))
+randomnum11=(random.randint(0, 6))
+randomnum12=(random.randint(0, 8))
 
 ship1smallx = enemyship_listx[randomnum1]
 ship1smally = enemyship_listy[randomnum2]
@@ -54,15 +53,14 @@ ship5mediumy = enemyship_listy[randomnum10]
 ship6smallx = enemyship_listx[randomnum11]
 ship6smally = enemyship_listy[randomnum12]
 
-enemyship_listx = [50,100,150,200,250,300,350,400,450,500,550]
-enemyship_listy = [50,100,150,200,250,300,350,400,450,500,550]
+enemyship_listx = [50,100,150,200,250,300,350,400,450,500]
+enemyship_listy = [50,100,150,200,250,300,350,400,450,500]
 
-shipxcoor= [ship1smallx, ship2mediumx, ship3largex,ship4largex, (ship4largex) + 50, (ship4largex) + 100, (ship5mediumx), (ship5mediumx)+50, ship6smallx]
-shipycoor = [ship1smally,ship2mediumy, (ship2mediumy) + 50, ship3largey, (ship3largey)+50, (ship3largey)+100, ship4largey, ship5mediumy, ship6smally]
+shipxcoor= [ship1smallx, ship2mediumx, ship3largex, ship4largex, ship5mediumx, ship6smallx]
+shipycoor= [ship1smally, ship2mediumy, ship3largey, ship4largey, ship5mediumy, ship6smally ]
 
-
-hitcoorx = [50,100,150,200,250,300,350,400,450,500,550]
-hitcoory = [50,100,150,200,250,300,350,400,450,500,550]
+hitcoorx = [50,100,150,200,250,300,350,400,450,500]
+hitcoory = [50,100,150,200,250,300,350,400,450,500]
 
 x = 0 #x used for x axis not to be confused with other x axis variables
 y = 0 #y is used for y axis not to be confused with other y axis variables
@@ -96,12 +94,12 @@ while run:
 
     pygame.draw.rect(win, (0, 100, 255), (550, 50, 500, 500))  # right side player ships and enemy hit
 
-    pygame.draw.rect(win, shiptestcolour, (ship1smallx, ship1smally, 50, 50))  # enemy first ship blends in
-    pygame.draw.rect(win, shiptestcolour, (ship2mediumx, ship2mediumy, 50, 100))
-    pygame.draw.rect(win,shiptestcolour, (ship3largex, ship3largey, 50, 150) )
-    pygame.draw.rect(win, shiptestcolour, (ship4largex,ship4largey,50, 150))
-    pygame.draw.rect(win, shiptestcolour, (ship5mediumx,ship5mediumy, 50, 100))
-    pygame.draw.rect(win, shiptestcolour, (ship6smallx,ship6smally, 50, 50))
+    pygame.draw.rect(win, shipfinalcolour, (ship1smallx, ship1smally, 50, 50))  # enemy first ship blends in
+    pygame.draw.rect(win, shipfinalcolour, (ship2mediumx, ship2mediumy, 50, 100))
+    pygame.draw.rect(win, shipfinalcolour, (ship3largex, ship3largey, 50, 150) )
+    pygame.draw.rect(win, shipfinalcolour, (ship4largex,ship4largey,150, 50))
+    pygame.draw.rect(win, shipfinalcolour, (ship5mediumx,ship5mediumy, 100, 50))
+    pygame.draw.rect(win, shipfinalcolour, (ship6smallx,ship6smally, 50, 50))
 
     pygame.draw.rect(win, black, ((mouse[0] // 50) * 50, (mouse[1] // 50) * 50, 50, 50))  # using mouse create ships
 
@@ -118,13 +116,45 @@ while run:
     if keys[pygame.K_SPACE]:
         new_variablex = x
         new_variabley = y
-        for i in range(0, 8):
-            if (hitcoorx[new_variablex] == shipxcoor[i]) and (hitcoory[new_variabley] == shipycoor[i]):
-                pygame.draw.rect(win, white, (hitcoorx[new_variablex], hitcoory[new_variabley], 50, 50))
-            else:
-                pygame.draw.rect(win, red, (hitcoorx[new_variablex], hitcoory[new_variabley], 50, 50))
-        print(hitcoorx[new_variablex], hitcoory[new_variabley])
-        print(shipxcoor[i], shipycoor[i])
+
+        if enemyship_listx[new_variablex] == ship1smallx and enemyship_listy[new_variabley] == ship1smally:
+            pygame.draw.rect(win, white, (hitcoorx[new_variablex], hitcoory[new_variabley], 50, 50))
+
+        elif enemyship_listx[new_variablex] == ship2mediumx and enemyship_listy[new_variabley] == ship2mediumy:
+            pygame.draw.rect(win, white, (hitcoorx[new_variablex], hitcoory[new_variabley], 50, 50))
+
+        elif enemyship_listx[new_variablex] == ship2mediumx and enemyship_listy[new_variabley] == ship2mediumy + 50:
+            pygame.draw.rect(win, white, (hitcoorx[new_variablex], hitcoory[new_variabley], 50, 50))
+
+        elif enemyship_listx[new_variablex] == ship3largex and enemyship_listy[new_variabley] == ship3largey:
+            pygame.draw.rect(win, white, (hitcoorx[new_variablex], hitcoory[new_variabley], 50, 50))
+
+        elif enemyship_listx[new_variablex] == ship3largex and enemyship_listy[new_variabley] == ship3largey + 50:
+            pygame.draw.rect(win, white, (hitcoorx[new_variablex], hitcoory[new_variabley], 50, 50))
+
+        elif enemyship_listx[new_variablex] == ship3largex and enemyship_listy[new_variabley] == ship3largey + 100:
+            pygame.draw.rect(win, white, (hitcoorx[new_variablex], hitcoory[new_variabley], 50, 50))
+
+        elif enemyship_listx[new_variablex] == ship4largex and enemyship_listy[new_variabley] == ship4largey:
+            pygame.draw.rect(win, white, (hitcoorx[new_variablex], hitcoory[new_variabley], 50, 50))
+
+        elif enemyship_listx[new_variablex] == ship4largex + 50 and enemyship_listy[new_variabley] == ship4largey:
+            pygame.draw.rect(win, white, (hitcoorx[new_variablex], hitcoory[new_variabley], 50, 50))
+
+        elif enemyship_listx[new_variablex] == ship4largex + 100 and enemyship_listy[new_variabley] == ship4largey:
+            pygame.draw.rect(win, white, (hitcoorx[new_variablex], hitcoory[new_variabley], 50, 50))
+
+        elif enemyship_listx[new_variablex] == ship5mediumx and enemyship_listy[new_variabley] == ship5mediumy:
+            pygame.draw.rect(win, white, (hitcoorx[new_variablex], hitcoory[new_variabley], 50, 50))
+
+        elif enemyship_listx[new_variablex] == ship5mediumx + 50 and enemyship_listy[new_variabley] == ship5mediumy:
+            pygame.draw.rect(win, white, (hitcoorx[new_variablex], hitcoory[new_variabley], 50, 50))
+
+        elif enemyship_listx[new_variablex] == ship6smallx and enemyship_listy[new_variabley] == ship6smally:
+            pygame.draw.rect(win, white, (hitcoorx[new_variablex], hitcoory[new_variabley], 50, 50))
+
+        else:
+            pygame.draw.rect(win, red, (hitcoorx[new_variablex], hitcoory[new_variabley], 50, 50))
 
     for i in range(50,551,50):   # grid system for left side
         pygame.draw.line(win, black, (i,50), (i,550), 5)
@@ -132,5 +162,6 @@ while run:
 
     win.blit(textsurface1, (400, 65))
     win.blit(textsurface2, (875, 65))
+    win.blit(textsurface3, (75, 10))
 
     pygame.display.update()
